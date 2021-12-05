e74ccae4ed61fe34c3daedb6e512ebf920cbd97c
Goku_ByMcDo <83100407+Goku-ByMcDo@users.noreply.github.com>
2021-12-05 11:07:04 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace FreeCsMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.essp = new System.Windows.Forms.Button();
+            this.glow = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wallhack = new System.Windows.Forms.Button();
+            this.triggerbot = new System.Windows.Forms.Button();
+            this.Aimbot = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 590);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // essp
+            // 
+            this.essp.BackColor = System.Drawing.Color.MediumOrchid;
+            this.essp.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.essp.Location = new System.Drawing.Point(12, 331);
+            this.essp.Name = "essp";
+            this.essp.Size = new System.Drawing.Size(168, 39);
+            this.essp.TabIndex = 1;
+            this.essp.Text = "ESP";
+            this.essp.UseVisualStyleBackColor = false;
+            this.essp.Click += new System.EventHandler(this.Nothing_Click);
+            // 
+            // glow
+            // 
+            this.glow.BackColor = System.Drawing.Color.MediumOrchid;
+            this.glow.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.glow.Location = new System.Drawing.Point(12, 396);
+            this.glow.Name = "glow";
+            this.glow.Size = new System.Drawing.Size(168, 41);
+            this.glow.TabIndex = 2;
+            this.glow.Text = "Glow";
+            this.glow.UseVisualStyleBackColor = false;
+            this.glow.Click += new System.EventHandler(this.glow_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 59);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Undetected";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // wallhack
+            // 
+            this.wallhack.BackColor = System.Drawing.Color.MediumOrchid;
+            this.wallhack.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.wallhack.Location = new System.Drawing.Point(12, 271);
+            this.wallhack.Name = "wallhack";
+            this.wallhack.Size = new System.Drawing.Size(168, 36);
+            this.wallhack.TabIndex = 4;
+            this.wallhack.Text = "Wallhack";
+            this.wallhack.UseVisualStyleBackColor = false;
+            this.wallhack.Click += new System.EventHandler(this.wallhack_Click);
+            // 
+            // triggerbot
+            // 
+            this.triggerbot.BackColor = System.Drawing.Color.MediumOrchid;
+            this.triggerbot.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.triggerbot.Location = new System.Drawing.Point(12, 205);
+            this.triggerbot.Name = "triggerbot";
+            this.triggerbot.Size = new System.Drawing.Size(168, 36);
+            this.triggerbot.TabIndex = 5;
+            this.triggerbot.Text = "Trigger-Bot";
+            this.triggerbot.UseVisualStyleBackColor = false;
+            this.triggerbot.Click += new System.EventHandler(this.esp_Click);
+            // 
+            // Aimbot
+            // 
+            this.Aimbot.BackColor = System.Drawing.Color.MediumOrchid;
+            this.Aimbot.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Aimbot.ForeColor = System.Drawing.Color.Black;
+            this.Aimbot.Location = new System.Drawing.Point(12, 147);
+            this.Aimbot.Name = "Aimbot";
+            this.Aimbot.Size = new System.Drawing.Size(168, 35);
+            this.Aimbot.TabIndex = 6;
+            this.Aimbot.Text = "Aimbot";
+            this.Aimbot.UseVisualStyleBackColor = false;
+            this.Aimbot.Click += new System.EventHandler(this.Aimbot_Click);
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.MediumOrchid;
+            this.title.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(12, 12);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(327, 57);
+            this.title.TabIndex = 7;
+            this.title.Text = "BTCheat";
+            this.title.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.Color.MediumOrchid;
+            this.linkLabel2.Font = new System.Drawing.Font("Segoe UI Black", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.linkLabel2.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.Location = new System.Drawing.Point(12, 487);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(215, 15);
+            this.linkLabel2.TabIndex = 9;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/Goku-ByMcDo";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 585);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.Aimbot);
+            this.Controls.Add(this.triggerbot);
+            this.Controls.Add(this.wallhack);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glow);
+            this.Controls.Add(this.essp);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BTCheat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button essp;
+        private System.Windows.Forms.Button glow;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button wallhack;
+        private System.Windows.Forms.Button triggerbot;
+        private System.Windows.Forms.Button Aimbot;
+        private System.Windows.Forms.Button title;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Configuration;
+
+
+
+
+
+namespace FreeCsMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string devby = "Goku_ByMcDo";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void esp_Click(object sender, EventArgs e)
+        {
+            string wanr1 = "TriggerBot !!!!!!";
+
+        }
+
+        private void Nothing_Click(object sender, EventArgs e)
+        {
+            string warn2 = "ESP !!!!!!";
+        }
+
+        private void Aimbot_Click(object sender, EventArgs e)
+        {
+            Process.Start("Firefox.exe");
+
+        }
+
+        private void wallhack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string no_content = "linkLabel1 -> No content -> don't delete this";
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("My github link -> https://github.com/Goku-ByMcDo");
+        }
+    }
+}
